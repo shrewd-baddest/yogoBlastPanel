452f00b00633c608d8191a9a5ad3607001f56d85
Paul Onsongo <paulonsongo365@gmail.com>
2026-01-20 13:56:47 +0300

    changes on db serverconfiguration

--- yogoblast2/backend/controllers/dbConnect.js
+++ yogoblast2/backend/controllers/dbConnect.js
@@ -7,16 +7,22 @@ console.log("DB USER:", process.env.DB_USER);
 console.log("DB PASSWORD:", process.env.DB_PASSWORD);
 
 // Create a pool instead of a single connection
+// const db = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 5432,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
 const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionString: process.env.DATABASE_URL
 });
+
+
 
 db.on('connect', () => {
   console.log('Connected to the database');
